a11dbfedb2b96577dc6244880a6a04d35d2b427f
Denis <denis.silva.ju@gmail.com>
2025-07-09 22:20:17 -0400

    ajustando componentes de servicos

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 // Variáveis utilitárias para o site institucional D&L Assistência Técnica
 export const CONTACT_PHONE = '(67) 98126-7455';
-export const CATALOG_LINK = 'https://wa.me/5567981267455'; // Exemplo: link para catálogo no WhatsApp
+export const CATALOG_LINK = 'https://wa.me/5567981267455';
 export const STORE_NAME = 'D&L Assistência Técnica';
 export const INSTAGRAM_LINK = 'https://instagram.com/dlassistenciatecnica';
 export const FACEBOOK_LINK = 'https://facebook.com/dlassistenciatecnica';
