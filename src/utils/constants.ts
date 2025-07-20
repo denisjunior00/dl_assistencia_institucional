89ba6265a3f1e50ccdd5589059283c7fbdb29577
Denis <denis.silva.ju@gmail.com>
2025-07-20 15:24:28 -0400

    ajustes finais

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,9 @@
 // Variáveis utilitárias para o site institucional D&L Assistência Técnica
-export const CONTACT_PHONE = '(67) 98126-7455';
-export const CATALOG_LINK = 'https://wa.me/5567981267455';
-export const STORE_NAME = 'D&L Assistência Técnica';
-export const INSTAGRAM_LINK = 'https://www.instagram.com/dl.assistencia_tecnica';
-export const FACEBOOK_LINK = 'https://facebook.com/dlassistenciatecnica';
-export const ADDRESS = 'Rua dos Marmadukes, 1234';
-export const CITY = 'Campo Grande, MS';
+export const CONTACT_PHONE      = '(67) 98126-7455';
+export const CATALOG_LINK       = 'https://wa.me/c/5567981267455';
+export const WHATSAPP_LINK      = 'https://wa.me/5567981267455';
+export const STORE_NAME         = 'D&L Assistência Técnica';
+export const INSTAGRAM_LINK     = 'https://www.instagram.com/dl.assistencia_tecnica';
+export const FACEBOOK_LINK      = 'https://facebook.com/dlassistenciatecnica';
+export const ADDRESS            = 'Rua dos Marmadukes, 1234';
+export const CITY               = 'Campo Grande, MS';
